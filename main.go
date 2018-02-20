b6ca6ae975e2b066c002388a896833851a9e54a9
Russ Cox <rsc@golang.org>
2018-02-20 11:23:30 -0500

    vgo: add doc comment

--- main.go
+++ main.go
@@ -2,6 +2,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Vgo is a prototype of what the go command
+// might look like with integrated support for package versioning.
+//
+// Download and install with:
+//
+//	go get -u golang.org/x/vgo
+//
+// Then run "vgo" instead of "go".
+//
+// See https://research.swtch.com/vgo-intro for an overview
+// and the documents linked at https://research.swtch.com/vgo
+// for additional details.
+//
+// This is still a very early prototype.
+// You are likely to run into bugs.
+// Please file bugs in the main Go issue tracker,
+// https://golang.org/issue,
+// and put the prefix `x/vgo:` in the issue title.
+//
+// Thank you.
+//
 package main
 
 import Main "cmd/go"
